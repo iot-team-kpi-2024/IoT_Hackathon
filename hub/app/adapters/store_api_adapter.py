fbb1299f0d5786976b59e9b437002a134c546c64
nikita5451 <118533998+nikita5451@users.noreply.github.com>
2024-03-13 20:16:37 +0200

    completed lab 3

--- hub/app/adapters/store_api_adapter.py
+++ hub/app/adapters/store_api_adapter.py
@@ -21,4 +21,11 @@ class StoreApiAdapter(StoreGateway):
         Returns:
             bool: True if the data is successfully saved, False otherwise.
         """
-        # Implement it
+        url = f"{self.api_base_url}/processed_agent_data/"
+
+        data = [processed_agent_data.model_dump_json() for processed_agent_data in processed_agent_data_batch]
+        response = requests.post(url, data='[' + ','.join(data) + ']', headers={'Content-Type': 'application/json'})
+        if response.status_code != 200:
+            logging.info(f"Invalid Hub response\nData: {data}\nResponse: {response}")
+            return False
+        return True
